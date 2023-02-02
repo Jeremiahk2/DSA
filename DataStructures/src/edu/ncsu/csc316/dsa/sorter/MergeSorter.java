773d7ab5c54d2684c7375561ff71889085325dbb
Jeremiah Knizley <jdknizle@ncsu.edu>
2023-02-02 17:55:16 -0500

    [Implement] Begin implementing mergeSorter

---
+++ DataStructures/src/edu/ncsu/csc316/dsa/sorter/MergeSorter.java
@@ -0,0 +1,45 @@
+package edu.ncsu.csc316.dsa.sorter;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * MergeSorter sorts arrays of comparable elements using the merge sort
+ * algorithm. This implementation ensures O(nlogn) worst-case runtime to sort an
+ * array of n elements that are comparable.
+ * 
+ * @author Dr. King
+ *
+ * @param <E> the type of elements to sort; elements must be {@link Comparable}
+ */
+public class MergeSorter<E extends Comparable<E>> extends AbstractComparisonSorter<E> {
+
+    /**
+     * Constructs a new MergeSorter with a specified custom Comparator
+     * 
+     * @param comparator a custom Comparator to use when sorting
+     */
+    public MergeSorter(Comparator<E> comparator) {
+        super(comparator);
+    }
+
+    /**
+     * Constructs a new MergeSorter with comparisons based on the element's natural
+     * ordering
+     */ 
+    public MergeSorter() {
+        this(null);
+    }
+
+	@Override
+	public void sort(E[] data) {
+		if (data.length >= 2) {
+			int mid = data.length / 2;
+			E[] left = Arrays.copyArray(data, 0, mid - 1);
+		}
+	}
+	
+	private void merge(E[] left, E[] right, E[] original) {
+		
+	}
+}
