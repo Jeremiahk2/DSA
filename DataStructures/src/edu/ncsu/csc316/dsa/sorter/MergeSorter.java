e222e7ea2fd78b9fe634369b61d0a58cb3b062c9
Jeremiah Knizley <jdknizle@ncsu.edu>
2023-02-02 18:17:03 -0500

    [Implementation][Test] Implemented and tested MergeSorter

--- DataStructures/src/edu/ncsu/csc316/dsa/sorter/MergeSorter.java
+++ DataStructures/src/edu/ncsu/csc316/dsa/sorter/MergeSorter.java
@@ -35,11 +35,28 @@ public class MergeSorter<E extends Comparable<E>> extends AbstractComparisonSort
 	public void sort(E[] data) {
 		if (data.length >= 2) {
 			int mid = data.length / 2;
-			E[] left = Arrays.copyArray(data, 0, mid - 1);
+			E[] left = Arrays.copyOfRange(data, 0, mid);
+			E[] right = Arrays.copyOfRange(data, mid, data.length);
+			
+			sort(left);
+			sort(right);
+			
+			merge(left, right, data);
 		}
 	}
 	
 	private void merge(E[] left, E[] right, E[] original) {
-		
+		int leftIndex = 0;
+		int rightIndex = 0;
+		while (leftIndex + rightIndex < original.length) {
+			if (rightIndex == right.length || leftIndex < left.length && compare(left[leftIndex], right[rightIndex]) < 0)  {
+				original[leftIndex + rightIndex] = left[leftIndex];
+				leftIndex = leftIndex + 1;
+			}
+			else {
+				original[leftIndex + rightIndex] = right[rightIndex];
+				rightIndex = rightIndex + 1;
+			}
+		}
 	}
 }
