b08327d6a25dbfbdc494cec462bc055395244ffc
Jeremiah Knizley <jdknizle@ncsu.edu>
2023-02-08 22:25:07 -0500

    [impement][test] Implemented and tested SkipListMap

---
+++ DataStructures/src/edu/ncsu/csc316/dsa/map/SkipListMap.java
@@ -0,0 +1,288 @@
+package edu.ncsu.csc316.dsa.map;
+
+import java.util.Comparator;
+import java.util.Random;
+
+/**
+ * A SkipListMap is an ordered (meaning entries are stored in a sorted order
+ * based on the keys of the entries) linked-memory representation of the Map
+ * abstract data type. This link-based map maintains several levels of linked
+ * lists to help approximate the performance of binary search using a
+ * linked-memory structure. SkipListMap ensures a O(logn) expected/average
+ * runtime for lookUps, insertions, and deletions.
+ *
+ * The SkipListMap class is based on algorithms developed for
+ * use with the textbook:
+ *
+ * Data Structures and Algorithms in Java, Sixth Edition Michael T. Goodrich,
+ * Roberto Tamassia, and Michael H. Goldwasser John Wiley and Sons, 2014
+ *
+ * @author Dr. King
+ *
+ * @param <K> the type of keys stored in the map
+ * @param <V> the type of values that are associated with keys in the map
+ */
+public class SkipListMap<K extends Comparable<K>, V> extends AbstractOrderedMap<K, V> {
+
+    /**
+     * Coin tosses are used when inserting entries into the data structure to ensure
+     * 50/50 probability that an entry will be added to the current level of the
+     * skip list structure
+     */
+    private Random coinToss;
+
+    /**
+     * Start references the topmost, leftmost corner of the skip list. In other
+     * words, start references the sentinel front node at the top level of the skip
+     * list
+     */
+    private SkipListNode<K, V> start;
+
+    /**
+     * The number of entries stored in the map
+     */
+    private int size;
+
+    /**
+     * The number of levels of the skip list data structure
+     */
+    private int height;
+
+    /**
+     * Constructs a new SkipListMap where keys of entries are compared based on
+     * their natural ordering based on {@link Comparable#compareTo}
+     */
+    public SkipListMap() {
+        this(null);
+    }
+
+    /**
+     * Constructs a new SkipListMap where keys of entries are compared based on a
+     * provided {@link Comparator}
+     *
+     * @param compare a Comparator that defines comparisons rules for keys in the
+     *                map
+     */
+    public SkipListMap(Comparator<K> compare) {
+        super(compare);
+        coinToss = new Random();
+        // Create a dummy head node for the left "-INFINITY" sentinel tower
+        start = new SkipListNode<K, V>(null);
+        // Create a dummy tail node for the right "+INFINITY" sentinel tower
+        start.setNext(new SkipListNode<K, V>(null));
+        // Set the +INFINITY tower's previous to be the "start" node
+        start.getNext().setPrevious(start);
+        size = 0;
+        height = 0;
+    }
+    
+    private SkipListNode<K, V> lookUp(K key) {
+        SkipListNode<K, V> current = start;
+        while (current.below != null) {
+            current = current.below;
+            while (!isSentinel(current.next) && compare(key, current.next.getEntry().getKey()) >= 0) {
+                current = current.next;
+            }
+        }
+        return current;
+    }
+
+    // Helper method to determine if an entry is one of the sentinel
+    // -INFINITY or +INFINITY nodes (containing a null key)
+    private boolean isSentinel(SkipListNode<K, V> node) {
+        return node.getEntry() == null;
+    }
+
+
+    @Override
+    public V get(K key) {
+        SkipListNode<K, V> temp = lookUp(key);
+        return temp.getEntry().getValue();
+    }
+
+    private SkipListNode<K, V> insertAfterAbove(SkipListNode<K, V> prev, SkipListNode<K, V> down, Entry<K, V> entry) {
+        SkipListNode<K, V> newNode = new SkipListNode<>(entry);
+        newNode.setBelow(down);
+        newNode.setPrevious(prev);
+        if (prev != null) {
+        	newNode.setNext(prev.getNext());
+        	newNode.getPrevious().setNext(newNode);
+        }
+        if (newNode.getNext() != null) {
+        	newNode.getNext().setPrevious(newNode);
+        }
+        if (down != null) {
+        	down.setAbove(newNode);
+        }
+    	return newNode;
+    }
+
+    @Override
+    public V put(K key, V value) {
+        SkipListNode<K, V> temp = lookUp(key);
+        if (!isSentinel(temp) && temp.getEntry().getKey().equals(key)) {
+        	V originalValue = temp.getEntry().getValue();
+        	while (temp != null) {
+        		MapEntry<K, V> mapTemp = (MapEntry<K, V>)temp.getEntry();
+        		mapTemp.setValue(value);
+        		temp = temp.getAbove();
+        	}
+        	return originalValue;
+        }
+        SkipListNode<K, V> q = null;
+        
+        int currentLevel = -1;
+        
+        do {
+        	currentLevel += 1;
+        	if (currentLevel >= height) {
+        		height += 1;
+        		SkipListNode<K, V> tail = start.next;
+        		start = insertAfterAbove(null, start, null);
+        		insertAfterAbove(start, tail, null);
+        	}
+        	q = insertAfterAbove(temp, q, new MapEntry<K, V>(key, value));
+        	
+        	while (temp.getAbove() == null) {
+        		temp = temp.getPrevious();
+        	}
+        	temp = temp.getAbove();
+        } while (coinToss.nextBoolean()); //true equals tails
+        size = size + 1;
+        return null;
+    }
+
+    @Override
+    public V remove(K key) {
+        SkipListNode<K, V> temp = lookUp(key);
+        V rtnValue = temp.getEntry().getValue();
+        if (!temp.getEntry().getKey().equals(key)) {
+        	return null;
+        }
+        while (temp != null) {
+        	SkipListNode<K, V> tempPrev = temp.prev;
+        	temp.prev = temp.next;
+        	temp.next.prev = tempPrev;
+        	temp.below = null;
+        	temp = temp.getAbove();
+        }
+        size--;
+        return rtnValue;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterable<Entry<K, V>> entrySet() {
+        EntryCollection set = new EntryCollection();
+        SkipListNode<K, V> current = start;
+        while (current.below != null) {
+            current = current.below;
+        }
+        current = current.next;
+        while (!isSentinel(current)) {
+            set.add(current.getEntry());
+            current = current.next;
+        }
+        return set;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("SkipListMap[");
+        SkipListNode<K, V> cursor = start;
+        while (cursor.below != null) {
+            cursor = cursor.below;
+        }
+        cursor = cursor.next;
+        while (cursor != null && !isSentinel(cursor) && cursor.getEntry().getKey() != null) {
+            sb.append(cursor.getEntry().getKey());
+            if (!isSentinel(cursor.next)) {
+                sb.append(", ");
+            }
+            cursor = cursor.next;
+        }
+        sb.append("]");
+
+        return sb.toString();
+    }
+
+    // This method may be useful for testing or debugging.
+    // You may comment-out this method instead of testing it, since
+    // the full string will depend on the series of random coin flips
+    // and will not have deterministic expected results.
+    public String toFullString() {
+        StringBuilder sb = new StringBuilder("SkipListMap[\n");
+        SkipListNode<K, V> cursor = start;
+        SkipListNode<K, V> firstInList = start;
+        while (cursor != null) {
+            firstInList = cursor;
+            sb.append("-INF -> ");
+            cursor = cursor.next;
+            while (cursor != null && !isSentinel(cursor)) {
+                sb.append(cursor.getEntry().getKey() + " -> ");
+                cursor = cursor.next;
+            }
+            sb.append("+INF\n");
+            cursor = firstInList.below;
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    private static class SkipListNode<K, V> {
+
+        private Entry<K, V> entry;
+        private SkipListNode<K, V> above;
+        private SkipListNode<K, V> below;
+        private SkipListNode<K, V> prev;
+        private SkipListNode<K, V> next;
+
+        public SkipListNode(Entry<K, V> entry) {
+            setEntry(entry);
+            setAbove(null);
+            setBelow(null);
+            setPrevious(null);
+            setNext(null);
+        }
+
+        public SkipListNode<K, V> getAbove() {
+            return above;
+        }
+
+        public Entry<K, V> getEntry() {
+            return entry;
+        }
+
+        public SkipListNode<K, V> getNext() {
+            return next;
+        }
+
+        public SkipListNode<K, V> getPrevious() {
+            return prev;
+        }
+
+        public void setAbove(SkipListNode<K, V> up) {
+            this.above = up;
+        }
+
+        public void setBelow(SkipListNode<K, V> down) {
+            this.below = down;
+        }
+
+        public void setEntry(Entry<K, V> entry) {
+            this.entry = entry;
+        }
+
+        public void setNext(SkipListNode<K, V> next) {
+            this.next = next;
+        }
+
+        public void setPrevious(SkipListNode<K, V> prev) {
+            this.prev = prev;
+        }
+    }
+}
---
+++ DataStructures/test/edu/ncsu/csc316/dsa/map/SkipListMapTest.java
@@ -0,0 +1,183 @@
+package edu.ncsu.csc316.dsa.map;
+
+import static org.junit.Assert.*;
+import java.util.Iterator;
+import org.junit.Before;
+import org.junit.Test;
+import edu.ncsu.csc316.dsa.data.Student;
+import edu.ncsu.csc316.dsa.map.Map.Entry;
+
+/**
+ * Test class for SearchTableMap
+ * Checks the expected outputs of the Map abstract data type behaviors when using
+ * a sorted array-based data structure that uses binary search to locate entries
+ * based on the key of the entry
+ *
+ * @author Dr. King
+ *
+ */
+public class SkipListMapTest {
+
+	/** integer/string map for testing purposes */
+    private Map<Integer, String> map;
+    /** Student/integer map for testing purposes */
+    private Map<Student, Integer> studentMap;
+    
+    /**
+     * Create a new instance of a search table map before each test case executes
+     */     
+    @Before
+    public void setUp() {
+        map = new SkipListMap<Integer, String>();
+        studentMap = new SkipListMap<Student, Integer>();
+    }
+
+    /**
+     * Test the output of the put(k,v) behavior
+     */     
+    @Test
+    public void testPut() {
+        assertEquals(0, map.size());
+        assertTrue(map.isEmpty());
+        assertNull(map.put(3, "string3"));
+        assertEquals(1, map.size());
+        
+        assertEquals("string3", map.put(3, "DifferentString"));
+        assertNull(map.put(4, "AnotherDifferentString"));
+    }
+
+    /**
+     * Test the output of the get(k) behavior
+     */     
+    @Test
+    public void testGet() {
+    	assertTrue(map.isEmpty());
+        assertNull(map.put(3, "string3"));
+        assertEquals("string3", map.get(3));
+        assertNull(map.put(5, "string5"));
+        assertEquals("string5", map.get(5));
+        assertNull(map.put(2, "string2"));
+        assertEquals("string2", map.get(2));
+        assertNull(map.put(4, "string4"));
+        assertEquals("string4", map.get(4));
+        assertNull(map.put(1, "string1"));
+        assertFalse(map.isEmpty());
+        
+        assertEquals("string1", map.get(1));
+    }
+
+    /**
+     * Test the output of the remove(k) behavior
+     */     
+    @Test
+    public void testRemove() {
+    	assertTrue(map.isEmpty());
+        assertNull(map.put(3, "string3"));
+        assertNull(map.put(5, "string5"));
+        assertNull(map.put(2, "string2"));
+        assertNull(map.put(4, "string4"));
+        assertNull(map.put(1, "string1"));
+        assertFalse(map.isEmpty());
+
+        
+        assertEquals("string3", map.remove(3));
+        
+        assertNull(map.remove(20));
+//        
+        assertEquals("string4", map.remove(4));
+//        assertEquals("SearchTableMap[1, 2, 5]", map.toString());
+    }
+    
+    /**
+     * Tests Map abstract data type behaviors to ensure the behaviors work
+     * as expected when using arbitrary objects as keys
+     */
+    @Test
+    public void testStudentMap() {
+        Student s1 = new Student("J", "K", 1, 0, 0, "jk");
+        Student s2 = new Student("J", "S", 2, 0, 0, "js");
+        Student s3 = new Student("S", "H", 3, 0, 0, "sh");
+        Student s4 = new Student("J", "J", 4, 0, 0, "jj");
+        Student s5 = new Student("L", "B", 5, 0, 0, "lb");
+        
+        assertTrue(studentMap.isEmpty());
+        assertNull(studentMap.put(s1, 1));
+        assertNull(studentMap.put(s2, 2));
+        assertNull(studentMap.put(s3, 3));
+        assertNull(studentMap.put(s4, 4));
+        assertNull(studentMap.put(s5, 5));
+    }
+    
+    /**
+     * Test the output of the iterator behavior, including expected exceptions
+     */ 
+    @Test
+    public void testIterator() {
+    	assertNull(map.put(3, "string3"));
+        assertNull(map.put(5, "string5"));
+        assertNull(map.put(2, "string2"));
+        assertNull(map.put(4, "string4"));
+        assertNull(map.put(1, "string1"));
+        Iterator<Integer> it = map.iterator();
+        assertTrue(it.hasNext());
+        assertThrows(UnsupportedOperationException.class, () -> it.remove());
+        
+        assertEquals("string1", map.get(it.next()));
+        assertEquals("string2", map.get(it.next()));
+        assertEquals("string3", map.get(it.next()));
+        assertThrows(UnsupportedOperationException.class, () -> it.remove());
+        assertEquals("string4", map.get(it.next()));
+        assertEquals("string5", map.get(it.next()));
+        assertFalse(it.hasNext());
+    }
+
+    /**
+     * Test the output of the entrySet() behavior, including expected exceptions
+     */     
+    @Test
+    public void testEntrySet() {
+    	assertNull(map.put(5, "string5"));
+        assertNull(map.put(4, "string4"));
+        assertNull(map.put(3, "string3"));
+        assertNull(map.put(2, "string2"));
+        assertNull(map.put(1, "string1"));
+        Iterable<Entry<Integer, String>> iterable = map.entrySet();
+        Iterator<Entry<Integer, String>> it = iterable.iterator();
+        assertTrue(it.hasNext());
+        assertThrows(UnsupportedOperationException.class, () -> it.remove());
+        
+        assertEquals("string1", it.next().getValue());
+        assertEquals( "string2", it.next().getValue());
+        assertEquals( "string3", it.next().getValue());
+        assertThrows(UnsupportedOperationException.class, () -> it.remove());
+        assertEquals("string4", it.next().getValue());
+        assertEquals( "string5", it.next().getValue());
+        assertFalse(it.hasNext());
+    }
+
+    /**
+     * Test the output of the values() behavior, including expected exceptions
+     */  
+    @Test
+    public void testValues() {
+    	assertNull(map.put(3, "string3"));
+        assertNull(map.put(5, "string5"));
+        assertNull(map.put(2, "string2"));
+        assertNull(map.put(4, "string4"));
+        assertNull(map.put(1, "string1"));
+        
+        Iterable<String> iterable = map.values();
+        Iterator<String> it = iterable.iterator();
+        
+        assertTrue(it.hasNext());
+        assertThrows(UnsupportedOperationException.class, () -> it.remove());
+        
+        assertEquals("string1", it.next());
+        assertEquals( "string2", it.next());
+        assertEquals( "string3", it.next());
+        assertThrows(UnsupportedOperationException.class, () -> it.remove());
+        assertEquals("string4", it.next());
+        assertEquals( "string5", it.next());
+        assertFalse(it.hasNext());
+    }
+}
